b182bd3fdf2421d0abd66deff40e853c9dff60c7
jazz <jazz@tiny1804.lan>
2025-04-16 23:53:18 +0200

    references

--- view/home.vue
+++ view/home.vue
@@ -4,6 +4,13 @@
         hello
         <p>This example demonstrates how a navigation menu on a mobile/smart phone could look like.</p>
         <p>Click on the hamburger menu (three bars) in the top right corner, to toggle the menu.</p>
+        <p>References
+            <ul>
+                <li><a href="https://github.com/kafkadev/vue-without-webpack/blob/master/docs/index.html">https://github.com/kafkadev/vue-without-webpack/blob/master/docs/index.html</a></li>
+                <li><a href="https://www.w3schools.com/howto/howto_js_mobile_navbar.asp">https://www.w3schools.com/howto/howto_js_mobile_navbar.asp</a></li>
+                <li><a href="https://github.com/alex-salnikov/vue-simple-menu">https://github.com/alex-salnikov/vue-simple-menu</a></li>
+            </ul>
+        </p>
     </div>
 </template>
 
